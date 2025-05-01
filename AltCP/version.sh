578e1268d2d473a0e3ea654967653ae651c4a637
externvoid <hrtanaka@gmail.com>
2025-05-01 14:18:46 +0900

    add: _Config.xcconfig

--- AltCP/version.sh
+++ AltCP/version.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd "$SRCROOT/$PRODUCT_NAME"
 current_date=$(date "+%Y%m%d")
-previous_build_number=$(awk -F "=" '/BUILD_NUMBER/ {print $2}' Config.xcconfig | tr -d ' ')
+previous_build_number=$(awk -F "=" '/BUILD_NUMBER/ {print $2}' _Config.xcconfig | tr -d ' ')
 
 previous_date="${previous_build_number:0:8}"
 counter="${previous_build_number:8}"
 
 new_counter=$((current_date == previous_date ? counter + 1 : 1))
 new_build_number="${current_date}${new_counter}"
-sed -i -e "/BUILD_NUMBER =/ s/= .*/= $new_build_number/" Config.xcconfig
+sed -i -e "/BUILD_NUMBER =/ s/= .*/= $new_build_number/" _Config.xcconfig
 
-rm -f Config.xcconfig-e
+rm -f _Config.xcconfig-e
